ec5c64ca4b08000216e3ee935bbad0eec9effb82
Andy <andyfransisko1@gmail.com>
2019-10-28 10:57:19 +0700

    Schedule GAS AJAX

--- application/views/Schedule/V_schedule.php
+++ application/views/Schedule/V_schedule.php
@@ -8,35 +8,84 @@
           <div class="col-md-6 order-md-last ftco-animate mt-5" data-scrollax=" properties: { translateY: '70%' }">
           </div>
           <div class=" col-md-20 d-none d-md-block">
-              
-    <div class="container1">
-		
-<center>
-		<div class="cal">
-			<div class="cal__header">
-				<button class="btn btn-action btn-link btn-lg" data-calendar-toggle="previous"><svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-				<path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path></svg></button>
-				<div class="cal__header__label" data-calendar-label="month">
-					March 2017
-				</div><button class="btn btn-action btn-link btn-lg" data-calendar-toggle="next"> <svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-				<path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path></svg></button>
-			</div>
-			<div class="cal__week">
-				<span>Mon</span> <span>Tue</span><span>Wed</span> <span>Thu</span> <span>Fri</span> <span>Sat</span> <span>Sun</span>
-			</div>
-			<div class="cal__body" data-calendar-area="month"></div>
-		</div>
-</center>q
-		<p class="demo-picked">
-			Date picked: <span data-calendar-label="picked"></span>
-		</p>
-
-		
-	</div>  
+               
           </div>
+           <div class="container1">
+            <center>
+                <div class="cal">
+                  <div class="cal__header">
+                    <button class="btn btn-action btn-link btn-lg" data-calendar-toggle="previous"><svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path></svg></button>
+                    <div class="cal__header__label" data-calendar-label="month">
+                      March 2017
+                    </div><button class="btn btn-action btn-link btn-lg" data-calendar-toggle="next"> <svg height="24" version="1.1" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path></svg></button>
+                  </div>
+                  <div class="cal__week">
+                    <span>Mon</span> <span>Tue</span><span>Wed</span> <span>Thu</span> <span>Fri</span> <span>Sat</span> <span>Sun</span>
+                  </div>
+                  <div class="cal__body" data-calendar-area="month"></div>
+                </div>
+            </center>
+          <p class="demo-picked">
+            Date picked: <span data-calendar-label="picked"></span>
+          </p>
+          </div> 
         </div>
           <div class="row "></div>
     </section>
+ 
+    <div id = "bawah" style="position:inherit;">
+      
+          <div class="container1">
+            <center>
+              <ul>
+                <li><span></span>
+                  <div>
+                    <div class="title">Codify</div>
+                    <div class="info">Let&apos;s make coolest things in css</div>
+                    <div class="type">Presentation</div>
+                  </div> <span class="number"><span>10:00</span> <span>12:00</span></span>
+                </li>
+                <li>
+                  <div><span></span>
+                    <div class="title">Codify</div>
+                    <div class="info">Let&apos;s make coolest things in javascript</div>
+                    <div class="type">Presentation</div>
+                  </div> <span class="number"><span>13:00</span> <span>14:00</span></span>
+                </li>
+                <li>
+                  <div><span></span>
+                    <div class="title">Codify</div>
+                    <div class="info">Let&apos;s make coolest things in css</div>
+                    <div class="type">Review</div>
+                  </div> <span class="number"><span>15:00</span> <span>17:45</span></span>
+                </li>
+                <li>
+                    <div><span></span>
+                      <div class="title">Codify</div>
+                      <div class="info">Let&apos;s make coolest things in css</div>
+                      <div class="type">Review</div>
+                    </div> <span class="number"><span>15:00</span> <span>17:45</span></span>
+                  </li>
+                  <li>
+                      <div><span></span>
+                        <div class="title">Codify</div>
+                        <div class="info">Let&apos;s make coolest things in css</div>
+                        <div class="type">Review</div>
+                      </div> <span class="number"><span>15:00</span> <span>17:45</span></span>
+                    </li>
+                    <li>
+                        <div><span></span>
+                          <div class="title">Codify</div>
+                          <div class="info">Let&apos;s make coolest things in css</div>
+                          <div class="type">Review</div>
+                        </div> <span class="number"><span>15:00</span> <span>17:45</span></span>
+                      </li>
+              </ul>
+          </div>
+      </center>  
+    </div> 
 
     <!-- Script -->
     <script src="<?php echo base_url() ?>assets/js/scriptnewcalendar.js"></script>
